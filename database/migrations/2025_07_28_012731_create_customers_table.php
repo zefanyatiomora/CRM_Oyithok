b91f1c86e9278a6bbb139bfa265817d56388dd06
zefanya <zefanyatiomora@gmail.com>
2025-07-28 08:31:24 +0700

    tabel customer

---
+++ database/migrations/2025_07_28_012731_create_customers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id('customer_id'); // Primary key
+            $table->string('customer_kode'); 
+            $table->string('customer_nama');
+            $table->text('customer_alamat')->nullable();
+            $table->string('customer_nohp', 20)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
