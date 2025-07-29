c164b97e3607eac947953ef6d92c944e7e057c42
Nairsya21 <nairsyasyauqighasila@gmail.com>
2025-07-29 11:21:16 +0700

    tambah layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.master');
 });
+Route::get('/', [WelcomeController::class, 'index']);
